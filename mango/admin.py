6e6b5906510b2b93ffa190be0395c9da423b7e02
Stripey <kevintrn99@gmail.com>
2019-10-31 18:36:46 -0400

    Finished database models

--- mango/admin.py
+++ mango/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from mango.models import userAccount, Account, Transactions
 # Register your models here.
+
+# This diplays the fields on the admin page
+class adminFields(admin.ModelAdmin):
+    list_display = ('email', 'username', 'userUUID', 'password', 'first_name', 'last_name')
+
+
+admin.site.register(userAccount, adminFields)
+admin.site.register(Account)
+admin.site.register(Transactions)
--- mango/models.py
+++ mango/models.py
@@ -19,16 +19,17 @@ class userAccountManager(BaseUserManager):
             password = password,
         )
         user.set_password(password)
-        user.save(user=self._db)
+        user.save(using=self._db)
         return user 
     
     def create_superuser(self, email, username, password):
-        user = self.model(
+        user = self.create_user(
             email = self.normalize_email(email),
             username = username,
             password = password,
         )
 
+        user.set_password(password)
         user.is_admin = True
         user.is_staff = True
         user.is_superuser = True
@@ -38,7 +39,7 @@ class userAccountManager(BaseUserManager):
 # Creation of custon User class to have userUUID
 class userAccount(AbstractBaseUser):
     # Required Fields
-    email = models.EmailField(verbose_name='email',max_length="128", unique=True)
+    email = models.EmailField(verbose_name='email', max_length=128 , unique=True)
     username = models.CharField(max_length=30, unique=True)
     date_joined = models.DateTimeField(verbose_name="date joined", auto_now_add=True)
     last_login = models.DateTimeField(verbose_name="last login", auto_now=True)
@@ -54,7 +55,7 @@ class userAccount(AbstractBaseUser):
     last_name = models.CharField(max_length=30)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
-    
+
     objects = userAccountManager()
 
     def __str__(self):
@@ -65,3 +66,31 @@ class userAccount(AbstractBaseUser):
 
     def has_module_perms(self, app_label):
         return True
+
+# Account Table
+class Account(models.Model):
+    account_ID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    account_PlaidID = models.CharField(max_length=128,unique=True, null=True, editable=False)
+    account_name = models.CharField(max_length=128)
+    account_type = models.CharField(max_length=128)
+
+    # This connects the account to a user, A user can have many accounts
+    user_id = models.ForeignKey(userAccount, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.account_name
+
+class Transactions(models.Model):
+    transaction_ID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    transaction_plaidID = models.CharField(max_length=128, unique=True, null=True, editable=False)
+    transaction_name = models.CharField(max_length=128, null=False)
+    transaction_amount = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    transaction_location = models.CharField(max_length=256, editable=True)
+    transaction_date = models.DateField(auto_now=False)
+
+    # The keys that its related to
+    account_ID = models.ForeignKey(Account, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(userAccount, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.transaction_name
